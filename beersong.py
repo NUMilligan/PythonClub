445084797ea5f53620bab78f586fd98c8960d19d
Martin Milligan <amm606@ads.northwestern.edu>
2019-07-23 16:50:04 -0500

    improved beersong

--- beersong.py
+++ beersong.py
@@ -1,18 +1,27 @@
-word = "bottles"
-for beer_num in range(99,0,-1):
-    print (beer_num,word,"of beer on the wall")
-    print (beer_num,word,"of beer")
-    print ("if one of those",word,"should happen to fall")
-    
-    if beer_num == 1:
-        print ("No more bottles of beer on the wall")
+unit = "carboy"
+substance = "sulfuric acid"
+location = "rack"
+
+def pluralize (word:str, quantity:int=2) -> str:
+    if quantity == 1:
+        retval=word
     else:
-        new_num = beer_num - 1
+        retval=word + "s"
 
-        if new_num == 1:
-            word = "bottle"
+    return retval
 
-        print (new_num,word,"of beer on the wall")
+for unit_num in range(99,0,-1):
+        print (unit_num,pluralize(unit,unit_num),"of",substance,"on the",location)
+        print (unit_num,pluralize(unit,unit_num),"of",substance)
+        
+        if unit_num == 1:
+            print ("If that",unit,"should happen to fall")
+            print ("There'll be no more",pluralize(unit),"of",substance,"on the",location)
+        else:
+            print ("If one of those",pluralize(unit,unit_num),"should happen to fall")
+            print ("That's",unit_num-1,pluralize(unit,unit_num-1),"of",substance,"on the",location)
+        
         print()
 
 print("The end")
+print()
