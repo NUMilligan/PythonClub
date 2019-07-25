14046b4f70ef05f0be9c37eb047823110abea92d
Martin Milligan <amm606@ads.northwestern.edu>
2019-07-25 17:33:32 -0500

    passing around functions as arguments

--- Song.py
+++ Song.py
@@ -1,5 +1,12 @@
 from TextHandler import TextHandler
 
+class Stanza: 
+    def __init__ (self) -> None:
+        self.lines = []
+
+    def add (self, line:str) -> None:
+        self.lines.append(line)
+
 class Song:
     def  __init__ (self, howmany:int = 99, unit:str = "carboy",substance:str = "muriatic acid", location:str = "rack", speaker = None) -> None:
         self.howmany = howmany
@@ -7,36 +14,82 @@ class Song:
         self.substance = substance
         self.location = location
         self.speaker = speaker
+        self.stanzas = []
+        self.title = ""
 
-    def display (self, text:str) -> None:
-        print (text)
+        self.compose()
 
-    def speak (self, text:str) -> None:
+    def speakline (self, text:str) -> None:
         if self.speaker != None:
             self.speaker.Speak (text)
 
-    def display_and_speak (self, text:str) -> None:
-        self.display(text)
-        self.speak(text)
+    def show (self) -> None:
+        print(self.title)
+        print()
+
+        for stanza in self.stanzas:
+            for line in stanza.lines:
+                print(line)
+            
+            print()
+        
+        print("The End")
+
+    def speak (self) -> None:
+        self.speakline(self.title)
+
+        for stanza in self.stanzas:
+            for line in stanza.lines:
+                self.speakline(line)
 
     def sing (self) -> None:
+        print(self.title)
+        self.speakline(self.title)
+        print()
+
+        for stanza in self.stanzas:
+            for line in stanza.lines:
+                print(line)
+                self.speakline(line)
+            
+            print()
+
+        print("The End")
+        print()
+
+    def perform (self, *args) -> None:
+        for func in args:
+            func(self.title)
+            func("")
+
+        for stanza in self.stanzas:
+            for line in stanza.lines:
+                for func in args:
+                    func(line)
+            
+            for func in args:
+                func("")
+
+        for func in args:
+            func("The End")
+
+    def compose (self) -> None:
         th = TextHandler()
 
-        self.display_and_speak(th.stringify ("The",th.pluralize(self.unit),"of",self.substance,"Song"))
-        print()
+        self.title = th.stringify ("The",th.pluralize(self.unit),"of",self.substance,"Song")
 
         for unit_num in range(self.howmany,0,-1):
-                self.display_and_speak(th.stringify (unit_num,th.pluralize(self.unit,unit_num),"of",self.substance,"on the",self.location))
-                self.display_and_speak(th.stringify (unit_num,th.pluralize(self.unit,unit_num),"of",self.substance))
+            stanza = Stanza()
 
-                if unit_num == 1:
-                    self.display_and_speak(th.stringify("If that",self.unit,"should happen to fall"))
-                    self.display_and_speak(th.stringify("There'll be no more",th.pluralize(self.unit),"of",self.substance,"on the",self.location))
-                else:
-                    self.display_and_speak(th.stringify ("If one of those",th.pluralize(self.unit,unit_num),"should happen to fall"))
-                    self.display_and_speak(th.stringify ("That's",unit_num-1,th.pluralize(self.unit,unit_num-1),"of",self.substance,"on the",self.location))
+            stanza.add(th.stringify (unit_num,th.pluralize(self.unit,unit_num),"of",self.substance,"on the",self.location))
+            stanza.add(th.stringify (unit_num,th.pluralize(self.unit,unit_num),"of",self.substance))
 
-                print()
-        
-        print("The end")
-        print()
+            if unit_num == 1:
+                stanza.add(th.stringify("If that",self.unit,"should happen to fall"))
+                stanza.add(th.stringify("There'll be no more",th.pluralize(self.unit),"of",self.substance,"on the",self.location))
+            else:
+                stanza.add(th.stringify ("If one of those",th.pluralize(self.unit,unit_num),"should happen to fall"))
+                stanza.add(th.stringify ("That's",unit_num-1,th.pluralize(self.unit,unit_num-1),"of",self.substance,"on the",self.location))
+
+            self.stanzas.append(stanza)
+    
--- byo_song.py
+++ byo_song.py
@@ -2,13 +2,18 @@ import win32com.client
 from Song import Song
 
 beer_song_params = { 'speaker': win32com.client.Dispatch("SAPI.SpVoice"),
-    'howmany': 3,
+    'howmany': 2,
     'unit': 'bottle',
     'substance': 'beer',
     'location': 'wall' }
 
-beer_song = Song(**beer_song_params)
-beer_song.sing()
+#beer_song = Song(**beer_song_params)
+#beer_song.sing()
 
-acid_song = Song(speaker = win32com.client.Dispatch("SAPI.SpVoice"),howmany = 2,unit = "carboy",substance = "muriatic acid",location = "rack")
-acid_song.sing()
+#acid_song = Song(speaker = win32com.client.Dispatch("SAPI.SpVoice"),howmany = 2,unit = "carboy",substance = "muriatic acid",location = "rack")
+#acid_song.say()
+
+# Provide a list of ways to output the song
+sound_output = win32com.client.Dispatch("SAPI.SpVoice")
+oil_song = Song(speaker = sound_output,howmany = 1,unit = "can",substance = "oil",location = "shelf")
+oil_song.perform(print,sound_output.Speak)
